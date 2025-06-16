bc2a500abe918902a1b4cb02c266d62249e97eed
ZecKa <18116930+zecka@users.noreply.github.com>
2025-06-16 14:56:25 +0200

    fix: __dirname is undefined on loadOrCreatConfig function

--- src/load-config.ts
+++ src/load-config.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 import inquirer from 'inquirer';
-import { pathToFileURL } from 'url';
+import { fileURLToPath, pathToFileURL } from 'url';
 
 import type { ReactIconsEjectConfig } from './types';
 
 const CONFIG_FILENAME = 'react-icon-eject.config.ts';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export async function loadOrCreateConfig(): Promise<ReactIconsEjectConfig> {
     const configPath = path.resolve(process.cwd(), CONFIG_FILENAME);
